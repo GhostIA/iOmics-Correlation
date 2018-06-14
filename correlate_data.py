f8c6ef0e96d2deca46471505a5c1d59723ee7ccc
sarahf5676 <sarahf5676@gmail.com>
2018-06-13 22:16:48 -0700

    Split up by ethnicity and gender

--- correlate_data.py
+++ correlate_data.py
@@ -2,32 +2,49 @@ import pandas as pd
 import numpy as np
 import sys
 
-print(sys.argv)
-
+# print(sys.argv)
+"""
 if len(sys.argv) < 7:
 	print("You didn't pass all parameters (6)")
-
-
-file_destination = sys.argv[1]
-file_destination2 = sys.argv[2]
+"""
+file_destination ="C:\\Users\\Groot\\Downloads\\phen-lipids-miRNA.xlsx"
+# file_destination2 = sys.argv[2]
 
 df4 = pd.read_excel(file_destination)
+"""
 df5 = pd.read_excel(file_destination2)
 df4 = pd.concat([df4, df5], axis=1, sort=False)
+"""
 df4.fillna(0).replace(" ",0)
-list_of_correlations = []
-correlation_constants = []
+def drop_x_row(df, x, column):
+     df_unwanted_row = df[df[column] == x]
+     df = df.drop(df_unwanted_row.index, axis=0)
+     return df
+
+one_values = drop_x_row(df4, 0, "gender adj")
+zero_values = drop_x_row(df4, 1, "gender adj")
+eth3_one = drop_x_row(one_values, 2, "Eth_Derived")
+eth3_one = drop_x_row(eth3_one, 1, "Eth_Derived")
+eth3_zero = drop_x_row(zero_values, 1, "Eth_Derived")
+eth3_zero = drop_x_row(eth3_zero, 2, "Eth_Derived")
+eth2_zero = drop_x_row(zero_values, 3, "Eth_Derived")
+eth2_zero = drop_x_row(eth2_zero, 1, "Eth_Derived")
+eth2_one = drop_x_row(one_values, 1, "Eth_Derived")
+eth2_one = drop_x_row(eth2_one, 3, "Eth_Derived")
+eth1_zero = drop_x_row(zero_values, 2, "Eth_Derived")
+eth1_zero = drop_x_row(eth1_zero, 3, "Eth_Derived")
+eth1_one = drop_x_row(one_values, 2, "Eth_Derived")
+eth1_one = drop_x_row(eth1_one, 3, "Eth_Derived")
+
 def correlate_data(df):
-    arr = list(df)
-    for i in range(sys.argv[3],sys.argv[4]): #miRNA to transcriptonomics
-        for j in range(sys.argv[5], sys.argv[6]):
-            corr = df[arr[j]].corr(df[arr[i]])
-            if corr >= 0.6 or corr <= -0.6:
-                print(arr[i])
-		print("to")
-		print(arr[j])
-                print(corr)
-               
-       
-correlate_data(df4)
- 
+     count = 0
+     arr = list(df)
+     for j in range(571, 844):
+          for i in range(288, 569):
+               corr = df[arr[i]].corr(df[arr[j]])
+               if corr >= 0.2:
+                    print(arr[i] + " to " + arr[j])
+                    print(corr)
+                    count = count +1
+     print(count)
+correlate_data(eth3_one)
