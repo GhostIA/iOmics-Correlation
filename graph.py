743041957a41c4bda7e891ff0dbd44ab11c2ee3a
sarahf5676 <39970608+sarahf5676@users.noreply.github.com>
2018-06-25 16:03:02 -0700

    Dropped rows and formed ethnicity

--- graph.py
+++ graph.py
@@ -3,16 +3,33 @@ import numpy as np
 from sklearn import svm, datasets
 import matplotlib.pyplot as plt 
 
+def drop_x_row(df, x, column):
+     df_true_false_values = df[df[column] == x]
+     df = df.drop(df_true_false_values.index, axis=0)
+     return df
+
 df = pd.read_excel("C:\\Users\\Groot\\Documents\\all data.xlsx", sheet = 1)
 df.fillna(0, inplace=True)
-X = df.loc[:,"[OPG]": "[OPN]"].copy()
-y = df.loc[:,"sii_comp_consensus_bp"].copy()
+df = drop_x_row(df, "hong kong", "country_of_birth")
+df = drop_x_row(df, "cambodia", "country_of_birth")
+df = drop_x_row(df, "china", "country_of_birth")
+df = drop_x_row(df, "vietnam", "country_of_birth")
+df = drop_x_row(df, "indonesia", "country_of_birth")
+df = drop_x_row(df, "korea", "country_of_birth")
+df = drop_x_row(df, "myanmar", "country_of_birth")
+df = drop_x_row(df, "india", "country_of_birth")
+df = drop_x_row(df, "phillipines", "country_of_birth")
+df.replace("malaysia", float(0.0), inplace = True)
+df.replace("singapore", float(1.0), inplace = True)
 
+X = df[["[TPO]","[IL-23]"]].copy()
+y = df.loc[:,"country_of_birth"].copy()
 
+print(y)
 
 x_min, x_max = X.iloc[:,0].min() + 1, X.iloc[:,0].max() +1
 y_min, y_max = X.iloc[:,1].min() + 1, X.iloc[:,1].max() +1
-print(y_max)
+print(x_min)
 h = (x_max / x_min)/100
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
  np.arange(y_min, y_max, h))
@@ -31,8 +48,9 @@ plt.subplot(121)
 plt.contourf(xx, yy, Z, cmap=plt.cm.tab10, alpha=0.3)
 plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, cmap=plt.cm.Set1)
 plt.xscale('linear')
-plt.xlabel('OPG')
-plt.ylabel('OPN')
-plt.xlim(xx.min(), xx.max())
+plt.xlabel('TPO')
+plt.ylabel('IL-23')
+plt.xlim(xx.min(), 500)
 plt.title('SVC with linear kernel')
 plt.show()
+
